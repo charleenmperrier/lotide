a10b496946c567de50c78b3bff7568b144473d72
charleenmperrier <charlee_perrier@hotmail.com>
2021-02-04 23:35:00 +0000

    added steps 4 and 5

--- eqObjects.js
+++ eqObjects.js
@@ -1,46 +1,75 @@
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log(`✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else (console.log(`🛑🛑 Assertion Failed: ${actual} !== ${expected}`));
+    console.log(`✅✅ assertEqual Assertion Passed: ${actual} === ${expected}`);
+  } else (console.log(`🛑🛑 assertEqual Assertion Failed: ${actual} !== ${expected}`));
+};
+
+const eqArrays = function(array1, array2) {
+  if (array1.length === array2.length) {
+    for (let i = 0; i < array1.length; i ++) {
+      if (array1[i] !== array2[i]) {
+        return false;
+      }
+    }
+  } else {
+    return false;
+  }
+  return true;
 };
 
 const eqObjects = function(object1, object2) {
   const objKeys1 = Object.keys(object1);
   const objKeys2 = Object.keys(object2);
 
-  // console.log(objKeys1, objKeys2)
-
-  if (objKeys1.length === objKeys2.length) {
-      // console.log('same length');
-    for (let key1 of objKeys1) {
-
-      //tried usig 2 for loops, but this was unessary in the end
-      // for (let i = 0; i < objKeys1.length; i++) {
-        // console.log(key1, key2)
-        // if ((key1 === key2)) {
-        //   console.log('matched keys')
-          if (object1[key1] !== object2[key1]) {
-            console.log('matched values')
-            return false
-          } 
-    }
   
-      return true
-    // } 
-    return false
+  if (objKeys1.length !== objKeys2.length) {
+    return false;
   }
-}
- 
+  for (let key1 of objKeys1) {
+    
+    if (Array.isArray(object1[key1]) && Array.isArray(object2[key1])) {
+      if (!eqArrays(object1[key1], object2[key1])) {
+        // console.log('console')
+        return false;
+      }
+    } else {
+      if (object1[key1] !== object2[key1]) {
+        // console.log(object1[key1], object2[key1])
+        
+        return false;
+      }
+    }
+  }
+  return true;
+};
+  
+const cd = {
+  c: "1",
+  d: ["2", 3]
+};
+const dc = {
+  d: ["2", 3],
+  c: "1"
+};
+const cd2 = {
+  c: "1",
+  d: ["2", 3, 4]
+};
+assertEqual(eqObjects(cd, dc), true); // => true
+assertEqual(eqObjects(cd, cd2), false); // => false
 
-const ab = { 
-  b: "2", 
-  a: "1" };
-const ba = { 
-  b: "2", 
-  a: "1" };
-const abc = { 
-  a: "1", 
-  b: "2", 
-  c: "3" };
-assertEqual(eqObjects(ab, ba), true); // => true
-assertEqual(eqObjects(ab, abc), false); // => false
+
+
+// const ab = {
+//   b: "2",
+//   a: "1" };
+// const ba = {
+//   b: "2",
+//   a: "1" };
+// const abc = {
+//   a: "1",
+//   b: "2",
+//   c: "3" };
+// assertEqual(eqObjects(ab, ba), true); // => true
+// assertEqual(eqObjects(ab, abc), false); // => false
+
