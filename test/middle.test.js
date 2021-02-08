c52248352ea4ce1f4c70b6594bd87c6e4448333b
charleenmperrier <charlee_perrier@hotmail.com>
2021-02-08 23:09:53 +0000

    added chai/mocha

--- test/middle.test.js
+++ test/middle.test.js
@@ -1,10 +1,37 @@
-const assertArraysEqual = require('../assertArraysEqual')
+const assert = require('chai').assert;
+// const assertArraysEqual = require('../assertArraysEqual')
 const middle = require('../middle');
 
 
-assertArraysEqual(middle([1]), [] ); // PASS
-assertArraysEqual(middle([1, 2]), []); // PASS
-assertArraysEqual(middle([1, 2]), [2]); // !PASS
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [2, 3]); // !PASS
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]); // PASS
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4, 5]); // !PASS
+
+
+describe("#head", () => {
+  it('should return [] from [1]', () => {
+    assert.deepEqual(middle([1]), [])
+  });
+
+  it('should return [] from [1, 2]', () => {
+    assert.deepEqual(middle([1, 2]), [])
+  });
+
+  it('should return [7] from [5, 6, 7, 8, 9]', () => {
+    assert.deepEqual(middle([5, 6, 7, 8, 9]), [7])
+  });
+
+  it('should return [7, 8] from [5, 6, 7, 8, 9, 10]', () => {
+    assert.deepEqual(middle([5, 6, 7, 8, 9, 10]), [7, 8])
+  });
+
+});
+
+
+
+
+
+
+// assertArraysEqual(middle([1]), [] ); // PASS
+// assertArraysEqual(middle([1, 2]), []); // PASS
+// assertArraysEqual(middle([1, 2]), [2]); // !PASS
+// assertArraysEqual(middle([1, 2, 3, 4, 5]), [2, 3]); // !PASS
+// assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]); // PASS
+// assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4, 5]); // !PASS
--- test/tail.test.js
+++ test/tail.test.js
@@ -1,7 +1,20 @@
+const assert = require('chai').assert;
 const tail = require('../tail')
-const assertEqual = require('../assertEqual');
 
 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
+
+
+describe("#head", () => {
+  it('should return ["Lighthouse", "Labs"] from ["Yo Yo", "Lighthouse", "Labs"]', () => {
+    assert.deepEqual(tail(["Yo Yo", "Lighthouse", "Labs"]), ["Lighthouse", "Labs"])
+  });
+
+  it('should return [2, 3, 4, 5] from [1, 2, 3, 4, 5]', () => {
+    assert.deepEqual(tail([1, 2, 3, 4, 5]), [2, 3, 4, 5])
+  });
+
+  it('should return [20, 30] from [10, 20, 30]', () => {
+    assert.deepEqual(tail([10, 20, 30]), [20, 30])
+  });
+
+});
